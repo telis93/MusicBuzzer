b9f8aefd56efa401fe16a84983f3c9f0f232a744
telis93 <dossas07@hotmail.com>
2013-12-07 16:26:44 +0200

    Add Components to RemoveFrame

--- MusicBuzzer/src/RemoveFrame.java
+++ MusicBuzzer/src/RemoveFrame.java
@@ -1,6 +1,9 @@
-import java.awt.HeadlessException;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.Container;
 
 import javax.swing.*;
 
@@ -11,13 +14,31 @@ public class RemoveFrame extends JFrame{
 	private JList<Note> list;
 	private JButton okButton;
 	private JButton removeButton;
+	private Container panel;
 
 	public RemoveFrame(Frame parent) {
 		super("Remove");
 		this.parent = parent;
 		list = new JList<Note>((Note[]) parent.getNotes().toArray(new Note[parent.getNotes().size()]));
+		list.setPreferredSize(new Dimension(150, 200));
 		okButton = new JButton("OK");
 		removeButton = new JButton("Remove");
+		panel = this.getContentPane();
+		
+		panel.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		c.gridx = 0;
+		c.gridy = 0;
+		c.gridheight = 5;
+		panel.add(list,c);
+		c.fill = GridBagConstraints.BOTH;
+		c.gridx = 1;
+		c.gridy = 0;
+		c.gridheight = 1;
+		panel.add(okButton,c);
+		c.gridx = 1;
+		c.gridy = 1;
+		panel.add(removeButton,c);
 		
 		this.setVisible(true);
 		this.pack();

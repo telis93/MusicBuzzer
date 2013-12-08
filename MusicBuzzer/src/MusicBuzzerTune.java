e4c887e4e33d6c5ae113071e1160b3cd967ce227
telis93 <dossas07@hotmail.com>
2013-12-08 19:59:18 +0200

    Create MusicBuzzerTune class

---
+++ MusicBuzzer/src/MusicBuzzerTune.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+import abc.notation.Tune;
+
+public class MusicBuzzerTune  extends Tune {
+	
+	private ArrayList<Note> notes;
+
+	public MusicBuzzerTune(ArrayList<Note> notes) {
+		super();
+		this.notes = notes;
+	}
+	
+
+}

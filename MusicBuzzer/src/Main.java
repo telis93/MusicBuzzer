71247795093d8445807d592d8a4dbe78a3cdb45f
telis93 <dossas07@hotmail.com>
2014-01-25 23:19:22 +0200

    Change LookAndFeel to system's

--- MusicBuzzer/src/Main.java
+++ MusicBuzzer/src/Main.java
@@ -1,3 +1,6 @@
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 
 public class Main {
 
@@ -5,7 +8,9 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {}
 		new Frame();
 
 	}
